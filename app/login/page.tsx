3a9d046706c00db8adca32ea2229f9d3becfd176
AndyPendragon <116746292+AndyPendragon@users.noreply.github.com>
2024-02-05 13:56:03 +0300

    feat: login page

---
+++ app/login/page.tsx
@@ -0,0 +1,10 @@
+import { title } from "@/components/primitives";
+import { Input } from "@nextui-org/input";
+
+export default function AboutPage() {
+	return (
+		<div>
+      <Input size={"md"} type="email" label="Email" placeholder="Enter your email" />
+		</div>
+	);
+}
