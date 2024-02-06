b67d4fe52ba6ec194699fe80fe0801c3ab048e94
AndyPendragon <116746292+AndyPendragon@users.noreply.github.com>
2024-02-06 08:58:55 +0300

    feat: login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,10 +1,43 @@
-import { title } from "@/components/primitives";
-import { Input } from "@nextui-org/input";
+"use client";
 
-export default function AboutPage() {
-	return (
-		<div>
-      <Input size={"md"} type="email" label="Email" placeholder="Enter your email" />
-		</div>
-	);
+import React from "react";
+import { EyeFilledIcon } from "@/components/EyeFilledIcon";
+import { EyeSlashFilledIcon } from "@/components/EyeSlashFilledIcon";
+import { Input } from "@nextui-org/input";
+import { title } from "@/components/primitives";
+import { Button } from "@nextui-org/button";
+
+export default function LoginPage() {
+  const [isVisible, setIsVisible] = React.useState(false);
+  const toggleVisibility = () => setIsVisible(!isVisible);
+  return (
+    <div className="flex flex-col gap-6">
+      <h1 className={title()}>Login</h1>
+      <div className="flex flex-col gap-2">
+        <Input variant="bordered" size={"lg"} type="email" label="Email" />
+        <Input
+          size={"lg"}
+          variant="bordered"
+          label="Password"
+          endContent={
+            <button
+              className="focus:outline-none"
+              type="button"
+              onClick={toggleVisibility}
+            >
+              {isVisible ? (
+                <EyeSlashFilledIcon className="text-2xl text-default-400 pointer-events-none" />
+              ) : (
+                <EyeFilledIcon className="text-2xl text-default-400 pointer-events-none" />
+              )}
+            </button>
+          }
+          type={isVisible ? "text" : "password"}
+        />
+        <Button color="primary" variant="shadow">
+          Let's go !
+        </Button>
+      </div>
+    </div>
+  );
 }
